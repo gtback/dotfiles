5621e23f17305164c20fee602858f0673fcee198
Greg Back <gtback@gmail.com>
2013-08-22 17:13:13 -0400

    Change how VIM plugins are installed.
    Use HTTPS rather than git: URLs, and don't try to create them again if
    they already exist.

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 # Install Pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+cd ~/.vim/autoload
+[[ -e pathogen.vim ]] || curl -Sso pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 # Install sensible and fugitive
 cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-sensible.git
-git clone git://github.com/tpope/vim-fugitive.git
+[[ -d syntastic ]] || git clone https://github.com/scrooloose/syntastic.git
+[[ -d vim-colors-solarized ]] || git clone https://github.com/altercation/vim-colors-solarized.git
+[[ -d vim-commentary ]] || git clone https://github.com/tpope/vim-commentary.git
+[[ -d vim-fugitive ]] || git clone https://github.com/tpope/vim-fugitive.git
+[[ -d vim-sensible ]] || git clone https://github.com/tpope/vim-sensible.git
+
 mkdir -p ~/.local/share/vim/undo
-git clone git://github.com/altercation/vim-colors-solarized.git
