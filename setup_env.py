f2e64df64e10e3c1247925c5a0006cdddfc8394b
Greg Back <gtback@gmail.com>
2018-04-07 23:39:58 -0500

    Update setup script for Python 3; clean up

--- setup_env.py
+++ setup_env.py
@@ -1,53 +1,51 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Symlink appropriate files into your home directory."""
 
 import os
 
+DOTFILES_DIR = os.path.dirname(os.path.realpath(__file__))
+HOME = os.getenv("HOME")
+
 # Global - add files you want to ignore in the current directory
 IGNORED = ["README.md", "_bashrc", ".git", "windows", "bundles.vim"]
 
-def main():
-    dotfiles_dir = os.path.dirname(os.path.realpath(__file__))
-    home = os.getenv("HOME")
 
-    dotfiles = os.listdir(dotfiles_dir)
+def link(filename, dest_dir=HOME):
+    source = os.path.join(DOTFILES_DIR, filename)
+    dest = os.path.join(dest_dir, filename).replace("_", ".")
+
+    if os.path.exists(dest):
+        print("Ignoring (already exists)")
+        return
+    try:
+        os.symlink(source, dest)
+        print("Linked to %s" % dest)
+    except OSError:
+        print("Failed (OSError)")
+        raise
+
+
+def main():
+    dotfiles = os.listdir(DOTFILES_DIR)
     dotfiles.sort()
 
     maxlen = max([len(s) for s in dotfiles])
     formatstr = "%%%ds -" % (maxlen + 2)
 
     for f in dotfiles:
-        print formatstr % f,
+        print(formatstr % f, end=" ")
         if f.startswith("setup_"):
-            print "Ignoring (setup script)"
-            continue
-
-        if f.endswith("~"):
-            print "Ignoring (temp file)"
-            continue
-
-        if f in IGNORED:
-            print "Ignoring (explict)"
-            continue
-
-        if f.startswith("_"):
-            source = os.path.join(dotfiles_dir, f)
-            dest = os.path.join(home, f).replace("_", ".")
-            if os.path.exists(dest):
-                print "Ignoring (already exists)"
-                continue
-
-            try:
-                os.symlink(source, dest)
-                print "Linked to %s" % dest
-            except OSError:
-                print "Failed (OSError)"
-                raise
-
+            print("Ignoring (setup script)")
+        elif f.endswith("~"):
+            print("Ignoring (temp file)")
+        elif f in IGNORED:
+            print("Ignoring (explict)")
+        elif f.startswith("_"):
+            link(f)
         else:
-            print "Ignoring (unexpected name)"
+            print("Ignoring (unexpected name)")
 
-    bashrc = os.path.join(home, ".bashrc")
+    bashrc = os.path.join(HOME, ".bashrc")
 
     bashrc_line = "source ~/dotfiles/_bashrc"
 
@@ -56,13 +54,14 @@ def main():
         with open(bashrc) as f:
             for line in f:
                 if line.strip() == bashrc_line:
-                    print "\n.bashrc has already been modified."
+                    print("\n.bashrc has already been modified.")
                     found = True
                     break
 
     if not found:
-        print "\nAdding dotfiles/.bashrc to the end of the ~/.bashrc"
+        print("\nAdding dotfiles/.bashrc to the end of the ~/.bashrc")
         os.system('echo "%s" >> $HOME/.bashrc' % bashrc_line)
+        
 
 if __name__ == "__main__":
     main()
