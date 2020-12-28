812dc6dff9686c436e88f84ef8a16f2dcf89e558
Greg Back <gtback@gmail.com>
2020-12-28 07:19:41 -0500

    Run shfmt on scripts

--- _fzf.zsh
+++ _fzf.zsh
@@ -1,12 +1,16 @@
+#!/bin/zsh
+# shellcheck shell=bash
+# Shellcheck doesn't support ZShell.
+
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+	export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2>/dev/null
 
 # Key bindings
 # ------------
