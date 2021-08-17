16296a1499bc8dadd0cb32c1fba83570a07826b8
Greg Back <gtback@gmail.com>
2021-08-16 22:49:08 -0400

    Vim improvements

--- setup_vim.sh
+++ setup_vim.sh
@@ -4,6 +4,6 @@
 mkdir -p ~/.vim/bundle
 [[ -d ~/.vim/bundle/Vundle.vim ]] || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-vim +BundleInstall +qall
+vim +PluginInstall! +qall
 
 mkdir -p ~/.local/share/vim/undo
