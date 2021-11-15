09ef55a8f3ce50f1d62cdf79449f9a238a3167ca
Greg Back <gregory.back@elastic.co>
2021-11-15 10:38:44 -0500

    Update k8s aliases

--- sh/k8s.sh
+++ sh/k8s.sh
@@ -15,16 +15,15 @@ alias kgn="kubectl get namespaces"
 alias kgno="kubectl get nodes"
 alias kgp="kubectl get pods"
 
-alias kunset="kubectl config unset current-context"
-
 function kn() {
     NAMESPACE=${1:-$(kubectl get namespaces | fzf | awk '{print $1}')}
     kubectl config set-context --current --namespace="$NAMESPACE"
 }
 
-alias k.nodeinfo="k get nodes -o=custom-columns=LAUNCH:.metadata.creationTimestamp,NAME:.metadata.name,VERSION:.status.nodeInfo.kubeletVersion --sort-by=.metadata.creationTimestamp"
+alias k.nodeinfo="kubectl get nodes -o=custom-columns=LAUNCH:.metadata.creationTimestamp,NAME:.metadata.name,VERSION:.status.nodeInfo.kubeletVersion --sort-by=.metadata.creationTimestamp"
+alias k.unset="kubectl config unset current-context"
 
 # https://github.com/jarpy/bin/blob/master/kpodnode
 function k.podnode() {
-    kubectl get pod $1 -o json | jq -r .spec.nodeName
+    kubectl get pod "$1" -o json | jq -r .spec.nodeName
 }
