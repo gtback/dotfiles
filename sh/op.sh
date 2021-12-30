7b9e9fba1d40d6d2b91aa3118f88c8da0479743e
Greg Back <git@gregback.net>
2021-12-30 10:47:37 -0500

    Make op.signin not prompt for password if signed in

--- sh/op.sh
+++ sh/op.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 function op.signin() {
-    eval "$(op signin "${OP_ACCOUNT}")"
+    if op.is-session-active; then
+        echo "op: already signed in"
+    else
+        eval "$(op signin "${OP_ACCOUNT}")"
+    fi
+}
+
+function op.is-session-active() {
+    # Exit code is 0 if active, 1 if inactive
+    op get account 2>/dev/null >&2
 }
