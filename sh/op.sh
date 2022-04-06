26c2e946954f1702f4df58921fecedd0416ce66a
Greg Back <git@gregback.net>
2022-04-05 22:09:10 -0400

    op.signin: re-prompt for password until successful

--- sh/op.sh
+++ sh/op.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# Log into the 1Password CLI (op).
+#
+# Unlike the `op signin` command, this function:
+# - checks if you're logged in first; this makes it easier to use in scripts
+#   without needing to type your master password multiple times.
+# - loops until a correct password is provided (or exited with CTRL-C)
 function op.signin() {
     if op.is-session-active; then
-        echo "op: already signed in"
+        echo >&2 "op: already signed in"
     else
-        echo "op: need to sign in"
-        eval "$(op signin "${OP_ACCOUNT}")"
+        echo >&2 "op: need to sign in"
+        while ! output=$(op signin "${OP_ACCOUNT}"); do
+            echo >&2 "incorrect password, try again"
+        done
+        echo >&2 "Signed in successfully"
+        eval "$output"
     fi
 }
 
