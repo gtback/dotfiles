28a8ffa92e5974385244324d8e3700c0166dbe43
Greg Back <gregory.back@elastic.co>
2022-11-03 15:01:29 -0400

    Move brew config to new file

---
+++ sh/darwin/brew.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export HOMEBREW_BUNDLE_NO_LOCK=1
+export HOMEBREW_CLEANUP_MAX_AGE_DAYS=14
+
+# https://docs.brew.sh/Analytics#opting-out
+export HOMEBREW_NO_ANALYTICS=1
+
+export HOMEBREW_BAT=1
+export HOMEBREW_INSTALL_BADGE="✅"
