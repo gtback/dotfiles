66115ae4c9d648def5c8a9bcbd14bf37f2ba4090
Greg Back <gregory.back@elastic.co>
2023-08-22 15:12:08 -0400

    Move mcfly configuration to separate file

---
+++ sh/mcfly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# McFly - fly through your shell history
+# https://github.com/cantino/mcfly
+
+# https://github.com/cantino/mcfly#vim-key-scheme
+export MCFLY_KEY_SCHEME=vim
+
+# https://github.com/cantino/mcfly#fuzzy-searching
+export MCFLY_FUZZY=2
+
+# https://github.com/cantino/mcfly#results-count
+export MCFLY_RESULTS=50
+
+# https://github.com/cantino/mcfly#delete-without-confirmation
+export MCFLY_DELETE_WITHOUT_CONFIRM=true
