521097711d185bc29d5169907e24c6fb4631866e
Greg Back <gregory.back@elastic.co>
2023-09-01 13:01:33 -0400

    Add script to compare installed and configured VS Code extensions

--- sh/code.sh
+++ sh/code.sh
@@ -5,17 +5,24 @@ code.dump-extensions() {
 }
 
 code.install-extensions() {
-    # 1. Parse the VSCode Extensions (jsonc) file into something that jq can
-    #    handle.
-    # 2. Extract the Recommended Extensions with jq.
-    # 3. Prefix each extension with `--install-extension`, so we can invoke
+    # 1. Parse the .extensions.json file
+    # 2. Prefix each extension with `--install-extension`, so we can invoke
     #    `code` just once rather than once per extension (with `xargs -n 1`).
     #    Massive performance improvement!
-    # 4. Invoke `code` to install all the extensions.
-    # 5. Strip out the part of lines that tells you how to force installing a specific version. We only care out the part of the output that says the extension is installed.
-    npx json5 <.vscode/extensions.json \
-        | jq -r '.recommendations[]' \
+    # 3. Invoke `code` to install all the extensions.
+    # 4. Strip out the part of lines that tells you how to force installing a
+    #    specific version. We only care out the part of the output that says the
+    #    extension is installed.
+    code.parse-extension-file \
         | awk '{print "--install-extension"; print $1}' \
         | xargs code \
         | sd " Use '--force'.*" ""
+}
+
+code.parse-extensions-file() {
+    # 1. Convert the VSCode Extensions (jsonc) file into something that jq can
+    #    handle.
+    # 2. Extract the Recommended Extensions with jq.
+    npx json5 <.vscode/extensions.json \
+        | jq -r '.recommendations[]'
 }
