62d2271faa4a3ef745ac9943b5a1fa7f3ca240d0
Greg Back <gregory.back@elastic.co>
2023-09-08 16:17:40 -0400

    Combine brew.sh files
    We don't need to load these functions on non-Darwin environments

--- sh/darwin/brew.sh
+++ sh/darwin/brew.sh
@@ -8,3 +8,17 @@ export HOMEBREW_NO_ANALYTICS=1
 
 export HOMEBREW_BAT=1
 export HOMEBREW_INSTALL_BADGE="✅"
+
+brew.find-all-brewfiles() {
+    # - Find all Brewfiles in code/ and dotfiles/
+    # - Find any Brewfiles in the home directory (if any exist)
+    fd Brewfile "${HOME}/code" "${HOME}/dotfiles"
+    fd --max-depth 1 Brewfile "${HOME}"
+}
+
+brew.find-brewfiles() {
+    # - Remove all files listed in the BREWFILE_IGNORE variable (typically used for
+    #   files for other hostnames)
+    brew.find-all-brewfiles \
+        | grep -E -v "${BREWFILE_IGNORE:-xxxxxx}"
+}
