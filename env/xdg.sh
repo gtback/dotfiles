68c5e989f64ab121eddcd2c7d54a1e0f17cefb3c
Greg Back <gregory.back@elastic.co>
2023-10-30 17:24:47 -0400

    Add Go build directory to PATH

--- env/xdg.sh
+++ env/xdg.sh
@@ -29,6 +29,7 @@ export TEXMFCONFIG="${XDG_CONFIG_HOME}/texlive/texmf-config"
 export _ZO_DATA_DIR="${XDG_DATA_HOME}/zoxide"
 
 export GOPATH="${XDG_DATA_HOME}/go"
+export PATH="${PATH}:${GOPATH}/bin/"
 
 export GRADLE_USER_HOME="${XDG_DATA_HOME}/gradle"
 
