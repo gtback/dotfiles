ee66adbe161999a3d47617500d806652304de738
Greg Back <gregory.back@elastic.co>
2024-10-04 17:05:03 -0400

    Add function to list all terraform resources in HCL file

--- sh/terraform.sh
+++ sh/terraform.sh
@@ -6,3 +6,12 @@ function tf.apply-workspace() {
     make apply
     popd || return
 }
+
+# List all resources defined in a Terraform HCL file, to make it easier to
+# create `-target` expressions for use with `terraform plan` and `terraform
+# apply`
+function tf.list-resources() {
+    # Use first filename, otherwise stdin
+    input=${1:--}
+    grep ^resource "$input" | sd '.*"(.*)" "(.*)".*' '$1.$2'
+}
