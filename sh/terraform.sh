a5f7d74ea1daa427d39255f6d1ddafcd6aa06538
Greg Back <gregory.back@elastic.co>
2024-11-22 15:17:38 -0500

    Update pre-commit config and fix warnings
    brew.up doesn't use ZSH anymore

--- sh/terraform.sh
+++ sh/terraform.sh
@@ -13,5 +13,8 @@ function tf.apply-workspace() {
 function tf.list-resources() {
     # Use first filename, otherwise stdin
     input=${1:--}
+
+    # Use `sd` to replace $1 and $2, rather than letting the shell expand them.
+    # shellcheck disable=SC2016
     grep ^resource "$input" | sd '.*"(.*)" "(.*)".*' '$1.$2'
 }
